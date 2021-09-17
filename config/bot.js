1d97bd5595f3fac8b756707cfea8d616e30db420
ModelVNN <modelvnn@gmail.com>
2021-09-17 20:37:57 +0700

    nice update

--- config/bot.js
+++ config/bot.js
@@ -1,9 +1,5 @@
 module.exports = {
-    emojis: {
-        pray: ':pray:'
-    },
-
     discord: {
-        token: 'ODg4MDc0NTYzMTY0OTc5MjAw.YUNZ4w.b6CZydrWtZY_aztfchhMb1fNIsg',
+        token: 'ODg4MDc0NTYzMTY0OTc5MjAw.YUNZ4w.ULzeLq0ZrLCc4fhudkMwjJhujzM',
     },
 };
